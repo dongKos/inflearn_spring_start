8a8943e91b10e101035eaf2f86971b392f15f96c
dongKos <dhkim931101@naver.com>
2023-04-15 14:42:25 +0900

    initial commit

---
+++ src/main/java/com/dongko/inflearn_spring_start/InflearnSpringStartApplication.java
@@ -0,0 +1,13 @@
+package com.dongko.inflearn_spring_start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnSpringStartApplication.class, args);
+    }
+
+}
